a623d99db11c6931309bedcb545129fada32ae0f
Ng Kah Lok <ngkahlok0407@gmail.com>
2025-09-18 16:58:31 +0800

    Prepare for deploment on render.com

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,18 +8,33 @@ import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import rateLimiter from './middleware/rateLimiter.js';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
-//configure CORS to allow requests from the frontend, then only rate limiting
-app.use(cors({
-    origin: 'http://localhost:5173', // replace with your frontend URL
-}));
+const __dirname = path.resolve(); // to get the current directory name
+
+if (process.env.NODE_ENV !== 'production') {
+    //configure CORS to allow requests from the frontend, then only rate limiting
+    app.use(cors({
+        origin: 'http://localhost:5173', // replace with your frontend URL
+    }));
+}
+
 app.use(express.json()); //middleware to parse JSON request body
 app.use(rateLimiter); // apply rate limiting middleware to all routes
 
 app.use("/api/notes", notesRoute);
+
+//run this only in production, render the frontend as static files
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+    });
+}
 
 connectDB().then(() => {
     app.listen(process.env.PORT, () => {
